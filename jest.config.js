b03edee57edb9d8becd3c0e8b3020686ad50fc64
tutorials2242 <githubfoundations123@gmail.com>
2025-10-18 07:25:00 +0500

    updated packages

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
+// jest.config.js
 module.exports = {
   testEnvironment: "node",
-  // ✅ Removed jest-circus/runner (no longer needed in Jest 30+)
-  // You can still add other options here:
   verbose: true,
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
-  testMatch: ["**/__tests__/**/*.test.js"],
+  testMatch: ["**/__tests__/**/*.test.js"]
 };
